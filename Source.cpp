015b54c533193c39ddfd5b321ea18fca3611539d
FarahAwad <78408663+FarahAwad03@users.noreply.github.com>
2022-07-12 21:11:26 +0200

    reading test file and saving data (FINAL)

--- Source.cpp
+++ Source.cpp
@@ -1,37 +1,50 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <sstream>
+#include <cmath>
 using namespace std;
 
 struct cacheLine
 {
-    string offset;
+    char validBit;
     string tag;
     string data;
 };
 
 char* readFile(string, int& fileSize );
-cacheLine* cacheInfo( char* file, int dataSize, int tagSize, int indexSize, int offsetSize, int fileSize);
+cacheLine** cacheInfo( char* file, int dataSize, int tagSize, int indexSize,int fileSize);
+
+const int n = 4;
+const int line_size = 32;
+const int CACHE_SIZE = 64 * 1024;
+const int ADDRESS_SIZE = 32;
+
+int byte_offset_bits = log2(line_size);
+int index_bits = log2(CACHE_SIZE / (n * line_size));
+int tag_bits = ADDRESS_SIZE - index_bits - byte_offset_bits;
+int lineNum = CACHE_SIZE / (n * line_size);
 
 int main()
 {
-    //Testing the functions
     int fileSize;
-    string fileName = "Text.txt";
+    string fileName = "Test_Case(4,32).txt";
     char* file = readFile(fileName, fileSize);
+    cacheLine** cache = new cacheLine * [lineNum];
 
-    cacheLine* cache = new cacheLine [5]; 
-    cache = cacheInfo(file, 4, 3, 8, 2, fileSize);
+    for (int i = 0; i < lineNum; i++)
+        cache[i] = new cacheLine[n];
+    cache = cacheInfo(file, line_size*8, tag_bits, lineNum, fileSize);
 
-    for (int i = 0; i <8 ; i++)
+    for (int i = 0; i < lineNum; i++)
     {
-        cout << cache[i].offset << " " << cache[i].tag << " " << cache[i].data << endl;
+        for (int j = 0; j < n; j++)
+            cout << cache[i][j].validBit << " " << cache[i][j].tag << " " << cache[i][j].data << endl;
     }
 
 	system("pause");
 	return 0;
 }
+
 char* readFile(string fileName, int& size)
 {
     char* file=nullptr;
@@ -50,36 +63,62 @@ char* readFile(string fileName, int& size)
     return file;
 
 }
-cacheLine* cacheInfo(char* file, int dataSize, int tagSize, int indexSize, int offsetSize, int fileSize)
+cacheLine** cacheInfo(char* file, int dataSize, int tagSize, int indexSize,int fileSize)
 {
-    string fileString, offsetTemp, tagTemp, dataTemp, fullLine;
-    int loopLimit = offsetSize + tagSize + dataSize + 3;
+    string fileString, tagTemp, dataTemp, fullLine;
+    char validBit;
+    int loopLimit = tagSize + dataSize + 4;
     int index = 0;
 
     for (int i = 0; i < fileSize; i++)
     {
         fileString += file[i];
     }
-
-    cacheLine *cache = new cacheLine[indexSize];
-    
-    for (int i = 0; i <= loopLimit*(indexSize-1); i=i+loopLimit)
+    cacheLine **cache = new cacheLine*[indexSize]; 
+    for (int i = 0; i < indexSize; i++)
+        cache[i] = new cacheLine[n];
+    int counter = 0;
+    string word = "";
+    int newline_ctr = 0;
+    int j = 0;
+    int test_ctr = 0;
+    for (int i = 0; i < fileString.size(); i++)
     {
-        fullLine= fileString.substr(i, offsetSize + tagSize + dataSize + 2);
-        tagTemp=fullLine.substr(offsetSize+1, tagSize);
-        dataTemp= fullLine.substr(offsetSize + tagSize + 2, dataSize);
-        offsetTemp = fullLine.substr(0, offsetSize);
-
-        cache[index].offset = offsetTemp;
-        cache[index].tag = tagTemp;
-        cache[index].data = dataTemp;
-        index++;
+            if (fileString[i] == '\n')
+            {
+                if (counter == 1)
+                    cache[index][j].tag = word;
+                else if (counter == 2)
+                    cache[index][j].data = word;
+                else if (!counter)
+                    cache[index][j].validBit = word[0];
+                j++;
+                test_ctr++;
+                counter = 0;
+                word = "";
+                if (j == n)
+                {
+                    index++;
+                    j = 0;
+                }
+            }
+            else if (fileString[i] == ' ')
+            {
+                if (counter == 1)
+                    cache[index][j].tag = word;
+                else if (counter == 2)
+                    cache[index][j].data = word;
+                else if (!counter)
+                {
+                    cache[index][j].validBit = word[0];
+                }
+                word = "";
+                counter++;
+            }
+            else
+                word = word + fileString[i];
+        
     }
-
-    /*for (int i = 0; i < 5; i++)
-    {
-        cout << cache[i].offset << "\t" << cache[i].tag << "\t" << cache[i].data << endl;
-    }*/
-   
+    cout << "Flag: " << test_ctr << endl;
     return cache;
 }
