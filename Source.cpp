7a7c0d6b1d11b959a7b9e3ef49b9baa2e249e574
FarahAwad <78408663+FarahAwad03@users.noreply.github.com>
2022-07-17 21:50:02 +0200

    2 validation txt files and FINAL source file

--- Source.cpp
+++ Source.cpp
@@ -3,11 +3,18 @@
 #include <string>
 #include <cmath>
 #include <iomanip>
+#include <cstdlib>
+#include "time.h"
+#include <set> 
 using namespace std;
 
-const int n = 1;
-const int line_size = 16; //16, 32, 64, 128  --> 4, 5, 6, 7
-const int CACHE_SIZE = 64 * 1024;
+set<int> sets;
+std::set<int>::iterator it1;
+std::set<int>::iterator it2;
+std::set<int>::iterator it3;
+const int n = 2;
+const int line_size = 4; //16, 32, 64, 128  --> 4, 5, 6, 7
+const int CACHE_SIZE = 64 ;
 const int ADDRESS_SIZE = 32;
 
 int byte_offset_bits = log2(line_size);
@@ -21,6 +28,7 @@ unsigned int NumMaskBits()
     unsigned int mask;
     switch (index_bits)
     {
+    case 3: mask = 0x7; break; //validation purposes
     case 6: mask = 0x3f; break;
     case 7: mask = 0x7f; break;
     case 8: mask = 0xff; break;
@@ -32,6 +40,15 @@ unsigned int NumMaskBits()
     }
     return mask;
 }
+unsigned int memGenValidate()
+{
+    static unsigned int addr = 0;
+    addr = rand() % 127;
+    //cout << addr << endl;
+    return addr;
+};
+
+
 unsigned int m_w = 0xABABAB55;    /* must not be zero, nor 0x464fffff */
 unsigned int m_z = 0x05080902;    /* must not be zero, nor 0x9068ffff */
 unsigned int rand_()
@@ -176,7 +193,13 @@ cacheLine** cacheInfo(char* file, int dataSize, int tagSize, int indexSize, int 
         else
             word = word + fileString[i];
     }
-    cout << "Flag: " << test_ctr << endl;
+    for (int i = 0; i < lineNum; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << cache[i][j].validBit << " " << cache[i][j].tag << endl;
+        }
+    }
     return cache;
 }
 
@@ -186,16 +209,26 @@ void read_from_cache(uint32_t address, int& HIT, int& MISS, cacheLine** cache) {
     // shifting to the left to get rid of the offset
     //unsigned int index = ExtractBitsFromUint(address, byte_offset_bits, byte_offset_bits + index_bits);
     unsigned int index = (address >> byte_offset_bits) & NumMaskBits();
-    //cout << "ReadFromCahce: index: " << index << endl;
+    
+
     // getting TAG
 
     // shifting to the left by 32 - #bits of index - #bits of offset to get rid of the offset and index
     int shifted_bits = index_bits + byte_offset_bits;
     unsigned int TAG = address >> shifted_bits;
-
+    cout << "Tag: " << TAG << endl;
+    cout << "Index: " << index << endl;
     //bitset<32> set = TAG;
-    string tagInString = DecToBin(TAG);
 
+    string tagInString = DecToBin(TAG);
+    cout << "TagInString: " << tagInString << "  " << tagInString.length() << endl;
+    int size = tagInString.size();
+    for (int i = 0; i < (tag_bits - size); i++)
+    {
+
+        tagInString = "0" + tagInString;
+    }
+    cout << tagInString << endl;
     // A boolean flag to check if we found the data inside the cache or not
     bool flag = false;
     unsigned int tag_replaced;
@@ -207,102 +240,80 @@ void read_from_cache(uint32_t address, int& HIT, int& MISS, cacheLine** cache) {
         //cout << "Cache elemnt Tag: " << cache[index][i].tag << endl;
         if (cache[index][i].tag == tagInString && cache[index][i].validBit == '1') // Data Found
         {
+            cout << "Line Found!\n";
             HIT++;
             flag = true;
             break;
         }
     }
+    bool flag2 = false;
     if (!flag) // Data isn't found
     {
         // writing the new tag inside the cache and valid_bit to 1
         // Note: We are doing it random as proved to be good
-
         // Generating a random number within the range of lines per set
-        do {
-            tag_replaced =(rand() % n);
-            counter++;
-        } while (cache[index][tag_replaced].validBit == '1' && counter != n);
-
-        cache[index][tag_replaced].tag = tagInString;
-        cache[index][tag_replaced].validBit = '1';
+        for (int i = 0; i < n; i++) {
+            if (cache[index][i].validBit == '0')
+            {
+                cache[index][i].tag = tagInString;
+                cache[index][i].validBit = '1';
+                cout << "Data stored!\n";
+                flag2 = true;
+                break;
+            }
+        }
+        if (!flag2)
+        {
+            tag_replaced = (rand() % n);
+            cache[index][tag_replaced].tag = tagInString;
+            cache[index][tag_replaced].validBit = '1';
+            cout << "Data stored after capacity miss (random insertion)!\n";
+        }
         MISS++;
-
     }
 }
 
 int main()
 {
     int fileSize;
-    string fileName = "Test_Case(1,16).txt";
+    //string fileName = "miss_set_2.txt"; 
+    string fileName = "fifty_set_2.txt";
+
     char* file = readFile(fileName, fileSize);
     auto cache = cacheInfo(file, line_size * 8, tag_bits, lineNum, fileSize);
+  
 
     int HIT = 0;
     int MISS = 0;
-    //// Testing
-    ////uint32_t address_1 = 0xB22A4000;//0x7345BFE1; // HIT
-    ////read_from_cache(address_1, HIT, MISS, cache);
-    ////address_1 = 0b101100100010101001; // HIT
-    //// read_from_cache(address_1, HIT, MISS, cache);
-    ///*uint32_t address_1 = 0xB22A4020;
-    //read_from_cache(address_1, HIT, MISS, cache);
-    //read_from_cache(address_1, HIT, MISS, cache);
-    //read_from_cache(address_1, HIT, MISS, cache);*/
-
     ofstream out;
     out.open(("exp1_16.csv"), ios::app);
     if (out.fail())
         cout << "file failed to open \n";
-
-
-
+    srand(time(0));
     uint32_t address;
-    cout << "Direct Mapped Cache Simulator\n";
-    int no_of_iterations = 1000000;
-    int limit= no_of_iterations /6;
+    //cout << "Tag: " << tag_bits << "Index: " << index_bits << "Bte off: " << byte_offset_bits << endl;
+    //cout << "Direct Mapped Cache Simulator\n";
+
+    int no_of_iterations = 16;
     for (int inst = 0; inst < no_of_iterations; inst++)
     {
-        if (0 <= inst && inst< limit)
-        {
-            address = memGen1();
-            read_from_cache(address, HIT, MISS, cache);
-            cout << "0x" << setfill('0') << setw(8) << hex << address << " (" << HIT << ")" << " (" << MISS << ") \n";
-        }
-        else if (limit <= inst && inst < limit*2)
-        {
-            address = memGen2();
-            read_from_cache(address, HIT, MISS, cache);
-            cout << "0x" << setfill('0') << setw(8) << hex << address << " (" << HIT << ")" << " (" << MISS << ") \n";
-        }
-        else if (limit*2 <= inst && inst < limit*3)
-        {
-            address = memGen3();
-            read_from_cache(address, HIT, MISS, cache);
-            cout << "0x" << setfill('0') << setw(8) << hex << address << " (" << HIT << ")" << " (" << MISS << ") \n";
-        }
-        else if (limit*3 <= inst && inst < limit*4)
-        {
-            address = memGen4();
-            read_from_cache(address, HIT, MISS, cache);
-            cout << "0x" << setfill('0') << setw(8) << hex << address << " (" << HIT << ")" << " (" << MISS << ") \n";
-        }
-        else if (limit*4 <= inst && inst < limit*5)
-        {
-            address = memGen5();
-            read_from_cache(address, HIT, MISS, cache);
-            cout << "0x" << setfill('0') << setw(8) << hex << address << " (" << HIT << ")" << " (" << MISS << ") \n";
-        }
-        else
-        {
-            address = memGen6();
-            read_from_cache(address, HIT, MISS, cache);
-            cout << "0x" << setfill('0') << setw(8) << hex << address << " (" << HIT << ")" << " (" << MISS << ") \n";
-        }
+        address = memGenValidate();
+        read_from_cache(address, HIT, MISS, cache);
+        cout << "0x" << setfill('0') << setw(8) << hex << address << " (" << HIT << ")" << " (" << MISS << ") \n";
     }
     cout << "Hit ratio = " << (100* HIT / no_of_iterations) << endl;
     cout << "HITS : " << HIT << "\n";
     cout << "MISS : " << MISS << "\n";
     out << line_size << "," << (100 * HIT / no_of_iterations) << endl;
+
+    for (int i = 0; i < lineNum; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << cache[i][j].validBit << " " << cache[i][j].tag << endl;
+        }
+    }
+
     system("pause");
     return 0;
 }
